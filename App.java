6c0837989fa8f55df2bd69d964e1ee86ee925446
chiara-p581 <chiaraagustina19@gmail.com>
2025-05-16 08:43:22 -0300

    Delete App.java

--- App.java
+++
@@ -1,7 +0,0 @@
-public class App extends MedioNotificador{
-
-    @Override
-    public void notificar(String mensaje, Cliente cliente) {
-        System.out.println("Enviando APP notification a " + cliente.getNombre() + ": " + mensaje);
-    }
-}
