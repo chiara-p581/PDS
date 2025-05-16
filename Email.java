1cabba4660d4baf2403a63551843bfb6937e8baa
chiara-p581 <chiaraagustina19@gmail.com>
2025-05-16 08:49:31 -0300

    Delete Email.java

--- Email.java
+++
@@ -1,7 +0,0 @@
-public class Email extends MedioNotificador {
-
-    @Override
-    public void notificar(String mensaje, Cliente cliente) {
-        System.out.println("Enviando EMAIL a " + cliente.getNombre() + ": " + mensaje);
-    }
-}
